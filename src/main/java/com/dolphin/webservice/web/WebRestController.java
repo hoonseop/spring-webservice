67baec50c05a20b8b24fb81909023b68e04b55f3
dolphin <corba13@gmail.com>
2019-05-04 02:37:36 +0900

    no message

---
+++ src/main/java/com/dolphin/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.dolphin.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+	@GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+}

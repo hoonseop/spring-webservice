67706cf823c8def2a60973d5e436fd2d01788834
dolphin <corba13@gmail.com>
2019-06-02 02:55:02 +0900

    db include

--- src/main/java/com/dolphin/webservice/Application.java
+++ src/main/java/com/dolphin/webservice/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-            + "classpath:real-db.yml";
+            + "classpath:db.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)

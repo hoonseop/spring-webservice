1bcc9cefda540347a2902dc745e5830a076cbd02
dolphin <corba13@gmail.com>
2019-06-21 16:21:14 +0900

    Roster create.

--- src/main/java/com/dolphin/webservice/domain/Roster.java
+++ src/main/java/com/dolphin/webservice/domain/Roster.java
@@ -1,5 +1,21 @@
 package com.dolphin.webservice.domain;
 
-public class Roster extends Player {
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Roster extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 16, nullable = false)
+    private String ownerId;
+
+    @Column(length = 16, nullable = false)    
+    private String playerId;
 
 }

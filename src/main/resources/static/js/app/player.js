c6c55d72ed48f9be27247a545f8ad7645bf4a50c
dolphin <corba13@gmail.com>
2019-07-01 10:34:48 +0900

    playerAll 추가, player 유지

--- src/main/resources/static/js/app/player.js
+++ src/main/resources/static/js/app/player.js
@@ -11,9 +11,7 @@ var player = {
         	playerName: $('#playerName').val(),
         	position: $('#position').val(),
             number: $('#number').val(),
-            salary: $('#salary').val(),
-            isEnroll: true
-//            isEnroll: $('#isEnroll').val()
+            salary: $('#salary').val()
         };
 
         $.ajax({
